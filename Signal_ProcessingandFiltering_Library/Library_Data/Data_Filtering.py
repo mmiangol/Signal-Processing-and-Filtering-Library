333e09c90d385e5add30b18a561fe8cb87e5a22d
Manu Miangolarra <147608532+mmiangol@users.noreply.github.com>
2023-10-14 17:39:58 +0200

    Update Data_Filtering.py

--- Signal_ProcessingandFiltering_Library/Library_Data/Data_Filtering.py
+++ Signal_ProcessingandFiltering_Library/Library_Data/Data_Filtering.py
@@ -4,27 +4,27 @@ import scipy.integrate as integrate
 
 
 #Low pass Butterworth filter function
-def Butterworth_filter_LP(sampling_Time, order, cut_off_frequency, Input_Signal):
+def Butterworth_filter_LP(sampling_time, order, cut_off_frequency, input_signal):
 
 
-    fs = 1/(sampling_Time)
+    fs = 1/(sampling_time)
     b, a = signal.butter(order, cut_off_frequency/(fs/2))
-    return signal.filtfilt(b, a, Input_Signal, axis=0)
+    return signal.filtfilt(b, a, input_signal, axis=0)
 
 
 #High pass Butterworth filter function
-def Butterworth_filter_HP(sampling_Time, order, cut_off_frequency, Input_Signal):
+def Butterworth_filter_HP(sampling_time, order, cut_off_frequency, input_signal):
 
 
-    fs = 1/(sampling_Time)
+    fs = 1/(sampling_time)
     b, a = signal.butter(order, cut_off_frequency/(fs/2), btype='highpass')
-    return signal.filtfilt(b, a, Input_Signal, axis=0)
+    return signal.filtfilt(b, a, input_signal, axis=0)
 
 #Numerical integration
-def Numerical_integration(Xdata, Ydata):
+def Numerical_integration(xdata, ydata):
     
- dxmean = np.mean(np.diff(Xdata, n=1, axis=0))
- Int_Y = integrate.cumtrapz(Ydata, None, dxmean, axis=0)
+ dxmean = np.mean(np.diff(xdata, n=1, axis=0))
+ Int_Y = integrate.cumtrapz(ydata, None, dxmean, axis=0)
  Int_Y = np.concatenate((np.array([0]), Int_Y), axis=0)
     
  return Int_Y
